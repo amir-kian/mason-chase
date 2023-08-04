fe4f65b05a2b2df7101c41b5a159c6b2518c1110
amir kian <a.kian32@yahoo.com>
2023-08-04 19:55:18 +0330

    add email and account number validation

--- Mc2.CrudTest.Domain/Entities/Customer.cs
+++ Mc2.CrudTest.Domain/Entities/Customer.cs
@@ -1,5 +1,5 @@
 ﻿using Mc2.CrudTest.Domain.ValueObjects;
-using PhoneNumbers;
+using System;
 
 namespace Mc2.CrudTest.Domain.Entities
 {
@@ -9,20 +9,25 @@ namespace Mc2.CrudTest.Domain.Entities
         public string Firstname { get; private set; }
         public string Lastname { get; private set; }
         public DateTime DateOfBirth { get; private set; }
-        public Mc2.CrudTest.Domain.ValueObjects.PhoneNumber PhoneNumber { get;  set; }
-        public Email Email { get;  set; }
-        public BankAccountNumber BankAccountNumber { get;  set; }
+        public PhoneNumber PhoneNumber { get; set; }
+        public Email Email { get; set; }
+        public BankAccountNumber BankAccountNumber { get; set; }
 
         private Customer() { }
 
-        public Customer(string firstname, string lastname, DateTime dateOfBirth, Mc2.CrudTest.Domain.ValueObjects.PhoneNumber phoneNumber, Email email, BankAccountNumber bankAccountNumber)
+        public Customer(string firstname, string lastname, DateTime dateOfBirth, PhoneNumber phoneNumber, Email email, BankAccountNumber bankAccountNumber)
         {
             Firstname = firstname;
             Lastname = lastname;
             DateOfBirth = dateOfBirth;
+
             ValidatePhoneNumber(phoneNumber);
             PhoneNumber = phoneNumber;
+
+            ValidateEmail(email);
             Email = email;
+
+            ValidateBankAccountNumber(bankAccountNumber);
             BankAccountNumber = bankAccountNumber;
         }
 
@@ -33,11 +38,11 @@ namespace Mc2.CrudTest.Domain.Entities
             DateOfBirth = dateOfBirth;
         }
 
-        private void ValidatePhoneNumber(Mc2.CrudTest.Domain.ValueObjects.PhoneNumber phoneNumber)
+        private void ValidatePhoneNumber(PhoneNumber phoneNumber)
         {
             if (phoneNumber != null)
             {
-                var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+                var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
                 var parsedPhoneNumber = phoneNumberUtil.Parse(phoneNumber.Value, "IR");
 
                 if (!phoneNumberUtil.IsValidNumber(parsedPhoneNumber))
@@ -45,6 +50,32 @@ namespace Mc2.CrudTest.Domain.Entities
                     throw new ArgumentException("Invalid phone number. Please provide a valid mobile number.");
                 }
             }
+        }
+
+        private void ValidateEmail(Email email)
+        {
+            if (email != null && !IsValidEmail(email.Value))
+            {
+                throw new ArgumentException("Invalid email address. Please provide a valid email address.");
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@");
+        }
+
+        private void ValidateBankAccountNumber(BankAccountNumber bankAccountNumber)
+        {
+            if (bankAccountNumber != null && !IsValidBankAccountNumber(bankAccountNumber.Value))
+            {
+                throw new ArgumentException("Invalid bank account number. Please provide a valid bank account number.");
+            }
+        }
+
+        private bool IsValidBankAccountNumber(string bankAccountNumber)
+        {
+            return !string.IsNullOrEmpty(bankAccountNumber);
         }
     }
 }
